8c4028d7aa1c812f89db74c3e6775a9bd4a2867b
Ghaith <ghaith990000@gmail.com>
2024-01-22 07:51:25 +0300

    show/hide completed tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent {
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items.filter(item => !item.complete);
+    return this.list.items.filter(item => this.showComplete || !item.complete);
   }
 
   addItem(newItem: string){
@@ -31,5 +31,6 @@ export class AppComponent {
       this.list.addItem(newItem);
     }
   }
-  title = 'todo';
+
+  showComplete: boolean = false;
 }
